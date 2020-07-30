cb8d7803db709cd1fb57f5f1eed874ceb28ebfa7
Hope Tambala <hopetambala@outlook.com>
2020-07-30 03:03:22 -0400

    Revert "feat: get offline login started"
    This reverts commit 52662fa5d5c7941db6b8dab710a3886dae0f7dbe.

--- modules/state-management/reducers/root-reducer.js
+++ modules/state-management/reducers/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import login from './login';
-import dashboardControls from './dashboard-controls';
+import dashboardControls from './dashboardControls';
 
 export default combineReducers({
   login,
