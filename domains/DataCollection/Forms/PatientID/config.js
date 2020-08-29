55793098759092c1ca9ac5d2d5086802f35c7151
Hope Tambala <hopetambala@outlook.com>
2020-08-29 17:36:00 -0400

    chore: add options for education

--- domains/DataCollection/Forms/PatientID/config.js
+++ domains/DataCollection/Forms/PatientID/config.js
@@ -77,6 +77,12 @@ const configArray = [
     value: '',
     fieldType: 'select',
     options: [
+      "lessThanprimary",
+      "primary",
+      "someHighSchool",
+      "highschool",
+      "someCollege",
+      "college"
     ]
   },
   {
