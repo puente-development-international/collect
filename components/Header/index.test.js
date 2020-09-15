901bbc926d10e9916d9972a1c8f2721a18521469
Hope Tambala <hopetambala@outlook.com>
2020-09-15 00:26:45 -0400

    chore: add unit and snapshot testing to header

---
+++ components/Header/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Header from './index';
+
+describe('<Header />', () => {
+  it('has 3 children - Unit Testing', () => {
+    const tree = renderer.create(<Header />).toJSON();
+    console.log(tree)
+    expect(tree.children.length).toBe(3);
+  });
+
+  it('renders correctly - Snapshot Testing', () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
