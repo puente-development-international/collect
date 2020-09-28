c1c7912c28184670fc47f9ce05ed07431bd0ce82
Hope Tambala <hopetambala@outlook.com>
2020-09-27 22:14:44 -0400

    chore: linting refactor

--- components/FormikFields/PaperInputPicker/HouseholdManager/index.js
+++ components/FormikFields/PaperInputPicker/HouseholdManager/index.js
@@ -11,64 +11,80 @@ import ResidentIdSearchbar from '../../../ResidentIdSearchbar';
 import { theme, layout } from '../../../../modules/theme';
 
 const relationships = [
-  'Parent',
-  'Sibling',
-  'Grand-Parent',
-  'Cousin',
-  'Other'
+  'Parent', 'Sibling', 'Grand-Parent', 'Cousin', 'Other'
 ];
 
 const HouseholdManager = (props) => {
   const {
     formikProps, formikKey
   } = props;
+  const { setFieldValue } = formikProps;
 
   const [selectPerson, setSelectPerson] = useState();
-  const [modalView, setModalView] = useState(false);
+  const [householdRelationship, setHouseholdRelationship] = useState();
+  const [modalView, setModalView] = useState('first');
+
+  const onSubmit = () => {
+    setModalView('third');
+    console.log(selectPerson);
+  };
+
+  const attachToExistingHousehold = () => {
+    // set householdId (from selectPerson) on the patientId
+    // setFieldValue(formikKey, result)
+  };
+
+  const createNewHousehold = () => {
+    // create new householdId and attach on the patientId
+    // setFieldValue(formikKey, result)
+
+  };
 
   return (
     <View style={layout.formContainer}>
-      {!modalView
+      {modalView === 'first'
         && (
           <View>
             <RadioButton.Group onValueChange={(value) => setModalView(value)} value={modalView}>
-              <View>
-                <Text>Yes, create a new household</Text>
-                <RadioButton value={false} />
-              </View>
-              <View>
-                <Text>No, link this individual to an existing house</Text>
-                <RadioButton value />
-              </View>
+              <RadioButton.Item label="Create a new household" value="first" />
+              <RadioButton.Item label="Link this individual to an existing house" value="second" />
             </RadioButton.Group>
           </View>
         )}
-      {modalView
+      {modalView === 'second'
         && (
           <Modal
             animationType="slide"
             visible
           >
             <Appbar.Header>
-              <Appbar.BackAction onPress={() => setModalView(false)} />
-              <Appbar.Content title="Household Manager" subtitle="Choose household members" />
+              <Appbar.BackAction onPress={() => setModalView('first')} />
+              <Appbar.Content title="Household Manager" subtitle="" />
             </Appbar.Header>
             <ResidentIdSearchbar
               selectPerson={selectPerson}
               setSelectPerson={setSelectPerson}
             />
-            <Text>What is their relationship?</Text>
+            <Text>What is their role/relationship in the household?</Text>
             <View style={layout.buttonGroupContainer}>
               {relationships.map((result) => (
-                <Button style={layout.buttonGroupButtonStyle} key={result} mode="outlined" onPress={() => formikProps.setFieldValue(formikKey, result)}>
+                <Button style={layout.buttonGroupButtonStyle} key={result} mode="outlined" onPress={() => setHouseholdRelationship(result)}>
                   <Text>{result}</Text>
                 </Button>
               ))}
             </View>
-            <Button theme={{ backgroundColor: theme.colors.primary }} style={layout.buttonGroupButtonStyle} mode="contained" onPress={() => setModalView(false)}>
+            <Button theme={{ backgroundColor: theme.colors.primary }} style={layout.buttonGroupButtonStyle} mode="contained" onPress={onSubmit}>
               <Text>Submit</Text>
             </Button>
           </Modal>
+        )}
+      {modalView === 'third'
+        && (
+          <View>
+            <RadioButton.Group onValueChange={(value) => setModalView(value)} value={modalView}>
+              <RadioButton.Item label="Linked" value="third" />
+            </RadioButton.Group>
+          </View>
         )}
     </View>
   );
