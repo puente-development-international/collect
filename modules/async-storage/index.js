81cc157c474c077dc846500692917e58e8c80924
Hope Tambala <hopetambala@outlook.com>
2020-10-27 15:47:45 -0400

    refactor: add failsafe for async-storage

--- modules/async-storage/index.js
+++ modules/async-storage/index.js
@@ -1,13 +1,23 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
 const storeData = async (value, storageName) => {
-  const jsonValue = JSON.stringify(value);
-  await AsyncStorage.setItem(storageName, jsonValue);
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(storageName, jsonValue);
+  } catch (e) {
+    // saving error
+    console.log(e); //eslint-disable-line
+  }
 };
 
 const getData = async (storageName) => {
-  const jsonValue = await AsyncStorage.getItem(storageName);
-  return jsonValue != null ? JSON.parse(jsonValue) : null;
+  try {
+    const jsonValue = await AsyncStorage.getItem(storageName);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log(e); //eslint-disable-line
+  }
+
 };
 
 const deleteData = async (storageName) => {
@@ -15,9 +25,13 @@ const deleteData = async (storageName) => {
 };
 
 const getAllData = async () => {
-  const keys = await AsyncStorage.getAllKeys();
-  const result = await AsyncStorage.multiGet(keys);
-  return result;
+  try {
+    const keys = await AsyncStorage.getAllKeys();
+    const result = await AsyncStorage.multiGet(keys);
+    return result;
+  } catch (e) {
+    console.log(e); //eslint-disable-line
+  }
 };
 
 export {
