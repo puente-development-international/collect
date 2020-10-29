3d740bd1d1a46b03636499e8797625b50aa2d1dc
Hope Tambala <hopetambala@outlook.com>
2020-10-29 02:00:46 -0400

    chore: add failsafe for screens

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { enableScreens } from 'react-native-screens';
+import { Platform } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
 
 import MainNavigation from './components/MainNavigation';
 
 import configureStore from './modules/state-management/configure-store';
-import useCachedResources from './modules/cached-resources/useCachedResources';
+// import useCachedResources from './modules/cached-resources/useCachedResources';
 import { theme } from './modules/theme';
 
 const store = configureStore();
-enableScreens();
+
+if (Platform.OS === 'android') {
+  enableScreens(true);
+}
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
+  // const isLoadingComplete = useCachedResources();
 
-  if (!isLoadingComplete) {
-    return null;
-  }
+  // if (!isLoadingComplete) {
+  //   return null;
+  // }
   return (
     <StoreProvider store={store}>
       <PaperProvider theme={theme}>
