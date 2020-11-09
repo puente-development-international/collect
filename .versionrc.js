25d1d1e3e5fa1eb5218ed51c9218bb8dab62438d
Hope Tambala <hopetambala@outlook.com>
2020-11-09 13:37:08 -0500

    docs: add ability to have better messages

--- .versionrc.js
+++ .versionrc.js
@@ -15,5 +15,17 @@ module.exports = {
       filename: 'app.json',
       updater: require.resolve('standard-version-expo/ios'),
     }
-  ]
+  ],
+  types: [
+    { type: 'chore', section: 'Other chores' },
+    { type: 'ci', section: 'Pipeline changes' },
+    { type: 'docs', section: 'Documentation changes' },
+    { type: 'feat', section: 'New features' },
+    { type: 'fix', section: 'Bug fixes' },
+    { type: 'perf', hidden: true },
+    { type: 'refactor', section: 'Code refactors' },
+    { type: 'release', hidden: true },
+    { type: 'style', hidden: true },
+    { type: 'test', hidden: true },
+  ],
 };
