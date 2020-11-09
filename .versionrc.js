d6b99c3c66c708c6d8ceae0f225ea3ed405ee06c
Hope Tambala <hopetambala@outlook.com>
2020-11-09 13:21:08 -0500

    chore: readme

--- .versionrc.js
+++ .versionrc.js
@@ -15,5 +15,15 @@ module.exports = {
       filename: 'app.json',
       updater: require.resolve('standard-version-expo/ios'),
     }
+  ],
+  types: [
+    { type: "feat", "section": "Features" },
+    { type: "fix", "section": "Bug Fixes" },
+    { type: "chore", "section": "Upkeep" },
+    { type: "docs", "hidden": true },
+    { type: "style", "hidden": true },
+    { type: "refactor", "hidden": true },
+    { type: "perf", "hidden": true },
+    { type: "test", "hidden": true }
   ]
 };
