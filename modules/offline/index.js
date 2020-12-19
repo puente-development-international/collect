eb5987eee92d651fd16fe259bebffe11372df493
Joseph McCombs <jamccomb92@gmail.com>
2020-12-19 15:27:57 -0500

    feat: android offline

--- modules/offline/index.js
+++ modules/offline/index.js
@@ -1,11 +1,26 @@
+import { Platform } from "react-native";
 import * as Network from 'expo-network';
+import NetInfo from "@react-native-community/netinfo";
 
 // checks whether user is connected to internet, return true if connected, false otherwise
 // this definitely works for ios, not tested with android
-const checkOnlineStatus = async () => {
-  const status = await Network.getNetworkStateAsync();
-  const { isConnected } = status;
-  return isConnected;
+const checkOnlineStatus = () => {
+  return new Promise(async (resolve, reject) => {
+    if (Platform.OS === 'ios') {
+      await Network.getNetworkStateAsync().then((status) => {
+        resolve(status.isConnected)
+      }, (error) => {
+        reject(error);
+      })
+    }
+    else {
+      NetInfo.fetch().then((state) => {
+        resolve(state.isConnected)
+      }, (error) => {
+        reject(error);
+      })
+    }
+  })
 };
 
 export default checkOnlineStatus;
